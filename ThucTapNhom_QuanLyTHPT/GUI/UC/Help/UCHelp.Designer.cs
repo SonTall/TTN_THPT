267962e159dae900e1167e5c9645dc57b8bdb478
Nguyễn Trung Nguyên <35571130+nguyenmta234@users.noreply.github.com>
2018-04-19 15:36:26 +0700

    helpp

---
+++ ThucTapNhom_QuanLyTHPT/GUI/UC/Help/UCHelp.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace ThucTapNhom_QuanLyTHPT.GUI.UC.Help
+{
+    partial class UCHelp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UCHelp));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelHS = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1155, 572);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1147, 546);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "View Help";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.labelHS);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1141, 540);
+            this.panel2.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(421, 380);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(296, 80);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "VIII. Chức năng Thống kê:\r\n1. Bấm vào nút thống kê\r\n2. Bạn sẽ xem được danh sách " +
+    "thống kê.\r\n3. Có thể Thêm, sửa, xóa ở các nút ấn.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(421, 263);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(304, 80);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "VII. Chức năng Bảng điểm:\r\n1. Bấm vào nút Bảng điểm\r\n2. Bạn sẽ xem được danh sách" +
+    " bảng điểm\r\n3. Có thể Thêm, sửa, xóa ở các nút ấn.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(421, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(306, 80);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "VI. Chức năng Giảng dạy:\r\n1. Bấm vào nút Giảng dạy\r\n2. Bạn sẽ xem được danh sách " +
+    "Giảng dạy.\r\n3. Có thể Thêm, sửa, xóa ở các nút ấn.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(14, 380);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(287, 80);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "IV. Chức năng lớp học:\r\n1. Bấm vào nút Lớp học\r\n2. Bạn sẽ xem được danh sách Lớp " +
+    "học\r\n3. Có thể Thêm, sửa, xóa ở các nút ấn.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(14, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(291, 80);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "III. Chức năng Chức vụ:\r\n1. Bấm vào nút Chức Vụ\r\n2. Bạn sẽ xem được danh sách Chứ" +
+    "c Vụ\r\n3. Có thể Thêm, sửa, xóa ở các nút ấn.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(421, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(295, 80);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "V. Chức năng môn học:\r\n1. Bấm vào nút Môn học\r\n2. Bạn sẽ xem được danh sách Môn h" +
+    "ọc.\r\n3. Có thể Thêm, sửa, xóa ở các nút ấn.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(14, 146);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 80);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "II. Chức năng giáo viên:\r\n1. Bấm vào nút Giáo viên\r\n2. Bạn sẽ xem được danh sách " +
+    " Giáo viên\r\n3. Có thể Thêm, sửa, xóa ở các nút ấn.";
+            // 
+            // labelHS
+            // 
+            this.labelHS.AutoSize = true;
+            this.labelHS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHS.Location = new System.Drawing.Point(14, 29);
+            this.labelHS.Name = "labelHS";
+            this.labelHS.Size = new System.Drawing.Size(293, 80);
+            this.labelHS.TabIndex = 0;
+            this.labelHS.Text = "I. Chức năng học sinh:\r\n1. Bấm vào nút Học sinh\r\n2. Bạn sẽ xem được danh sách học" +
+    " sinh.\r\n3. Có thể Thêm, sửa, xóa ở các nút ấn.";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage2.Controls.Add(this.label9);
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1147, 546);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "About Student";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(529, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(92, 90);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("UTM Androgyne", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(322, 145);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(519, 69);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Students Management";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("UTM Helve", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(511, 249);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(146, 198);
+            this.label9.TabIndex = 2;
+            this.label9.Text = resources.GetString("label9.Text");
+            // 
+            // UCHelp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1155, 572);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "UCHelp";
+            this.Text = "UCHelp";
+            this.Load += new System.EventHandler(this.UCHelp_Load_1);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelHS;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ ThucTapNhom_QuanLyTHPT/GUI/UC/Help/UCHelp.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThucTapNhom_QuanLyTHPT.GUI.UC.Help
+{
+    public partial class UCHelp : Form
+    {
+        public UCHelp()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_MouseClick(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void UCHelp_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            //About_Help about_help = new About_Help();
+          //  about_help.Show();
+        }
+        private void btnAbout_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnViewHelp_Click(object sender, EventArgs e)
+        {
+            
+          
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UCHelp_Load_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void linkLabel7_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnAbout_Click_1(object sender, EventArgs e)
+        {
+            
+            
+        }
+    }
+}
